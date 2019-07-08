42507d09040a04b7b347ca14c80bb6dbf3f2c00c
Dicu Razvan Gabriel <razvan.dicu@itsix.com>
2019-07-08 14:16:19 +0300

    init

---
+++ server/index.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var http = require('http')
+var socketio = require('socket.io');
+var mongojs = require('mongojs');
+
+var ObjectID = mongojs.ObjectID;
+var app = express();
+var server = http.Server(app);
+var websocket = socketio(server);
+server.listen(3000, () => console.log('listening on *:3000'));
+
+// Mapping objects to easily map sockets and users.
+var clients = {};
+var users = {};
+
+// This represents a unique chatroom.
+// For this example purpose, there is only one chatroom;
+var chatId = 1;
+
+websocket.on('connection', (socket) => {
+    clients[socket.id] = socket;
+    socket.on('userJoined', (userId) => onUserJoined(userId, socket));
+    socket.on('message', (message) => onMessageReceived(message, socket));
+});
+
+// Event listeners.
+// When a user joins the chatroom.
+function onUserJoined(userId, socket) {
+  try {
+    // The userId is null for new users.
+    if (!userId) {
+        socket.emit('userJoined', user._id);
+        users[socket.id] = user._id;
+        _sendExistingMessages(socket);
+    } else {
+      users[socket.id] = userId;
+      _sendExistingMessages(socket);
+    }
+  } catch(err) {
+    console.err(err);
+  }
+}
+
+// When a user sends a message in the chatroom.
+function onMessageReceived(message, senderSocket) {
+  var userId = users[senderSocket.id];
+  // Safety check.
+  if (!userId) return;
+
+  _sendAndSaveMessage(message, senderSocket);
+}
+
+// Helper functions.
+// Send the pre-existing messages to the user that just joined.
+function _sendExistingMessages(socket) {
+
+  if (!messages.length) return;
+      socket.emit('message', messages.reverse());
+}
+
+function _sendAndSaveMessage(message, socket, fromServer) {
+  var messageData = {
+    text: message.text,
+    user: message.user,
+    createdAt: new Date(message.createdAt),
+    chatId: chatId
+  };
+
+    var emitter = fromServer ? websocket : socket.broadcast;
+    emitter.emit('message', [message]);
+}
+
+// Allow the server to participate in the chatroom through stdin.
+var stdin = process.openStdin();
+stdin.addListener('data', function(d) {
+  _sendAndSaveMessage({
+    text: d.toString().trim(),
+    createdAt: new Date(),
+    user: { _id: 'robot' }
+  }, null /* no socket */, true /* send from server */);
+});
